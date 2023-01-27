880c63913a0b8ad6cc8d303980c983b2372fd433
fullerak <fullerak@go.stockton.edu>
2023-01-26 20:59:48 -0500

    testing going into dev before going into master

--- ProbAndStats/src/StatsLibraryTester.java
+++ ProbAndStats/src/StatsLibraryTester.java
@@ -6,6 +6,7 @@ public class StatsLibraryTester {
 		//Testing making a change and seeing if it will work
 		//im seeing how this works from now on
 		//this isnt working
+		//let me test going into dev
 
 	}
 
