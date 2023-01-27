647ff0de093d4021f2faa809383901982502f9e0
fullerak <fullerak@go.stockton.edu>
2023-01-26 20:55:55 -0500

    this is a second test, after i made another branch

--- ProbAndStats/src/StatsLibraryTester.java
+++ ProbAndStats/src/StatsLibraryTester.java
@@ -4,6 +4,7 @@ public class StatsLibraryTester {
 	public static void main(String[] args) {
 		
 		//Testing making a change and seeing if it will work
+		//im seeing how this works from now on
 
 	}
 
