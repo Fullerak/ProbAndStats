7e845939339a9096a0519080d497ff6f8da593fa
GameUnmaker <damastazen@gmail.com>
2023-02-01 13:17:14 -0500

    Addind a comment to doorproblem

--- ProbAndStats/src/DoorProblem.java
+++ ProbAndStats/src/DoorProblem.java
@@ -3,6 +3,7 @@ public class DoorProblem {
 
 	//write a program to emulate the 3 door game
 	//write a version to emulate the game when one wrong door is taken away
+	//hello, i think i did the branch thing wrong
 	
 	public DoorProblem() {
 		
