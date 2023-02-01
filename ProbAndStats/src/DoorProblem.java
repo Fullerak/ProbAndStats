81043e4dec231ad1ddf36cf9a3bbb3f66571739d
fullerak <fullerak@go.stockton.edu>
2023-01-31 19:57:25 -0500

    new classes

--- ProbAndStats/src/DoorProblem.java
+++ ProbAndStats/src/DoorProblem.java
@@ -1,4 +1,14 @@
 
 public class DoorProblem {
 
+	//write a program to emulate the 3 door game
+	//write a version to emulate the game when one wrong door is taken away
+	
+	public DoorProblem() {
+		
+	}
+	
+	
+	
+	
 }
--- ProbAndStats/src/DoorProblemTester.java
+++ ProbAndStats/src/DoorProblemTester.java
@@ -2,7 +2,8 @@
 public class DoorProblemTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
 
