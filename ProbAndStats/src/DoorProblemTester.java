a883069858e93083d8c79cb3957369537821d464
fullerak <fullerak@go.stockton.edu>
2023-03-22 13:16:06 -0400

    testing key

--- ProbAndStats/src/DoorProblemTester.java
+++ ProbAndStats/src/DoorProblemTester.java
@@ -7,11 +7,11 @@ public class DoorProblemTester {
 
 	public static void main(String[] args) {
 		
+		//creating a doorProblem and testing a switch and non switch of the door
 		DoorProblem test = new DoorProblem();
-		System.out.println("The probability of you winning if you don't change your door is:     " + test.montyProblem() + "%");
-		System.out.println("The probability of you winning if you cahnge your door is:           " + test.montyProblemSwitch() + "%");
+		test.returnResult();
 	}
 		
 }
 
-
+//
